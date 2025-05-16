948c2da8f051651840572505ef3ea60c9a64a0f8
Administrator <Administrator@B508PC12>
2025-05-16 15:35:24 +0700

    Add project files.

---
+++ Cau 2/CheckingAccountForm.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cau_2
+{
+    public partial class CheckingAccountForm : Form
+    {
+        public CheckingAccountForm()
+        {
+            InitializeComponent();
+        }
+        private readonly BankAccountService _accountService;
+        private readonly BankAccount _account;
+        public CheckingAccountForm(BankAccountService accountService, BankAccount account)
+        {
+            InitializeComponent();
+            _accountService = accountService;
+            _account = account;
+            DisplayAccountInfo();
+        }
+
+        private void DisplayAccountInfo()
+        {
+            lblAccountId.Text = _account.AccountId.ToString();
+            lblOwnerName.Text = _account.OwnerName;
+            lblBalance.Text = _account.Balance.ToString("C");
+        }
+
+        private void btnDeposit_Click(object sender, EventArgs e)
+        {
+            var depositDialog = new DepositForm();
+            if (depositDialog.ShowDialog() == DialogResult.OK)
+            {
+                double amount = depositDialog.Amount;
+                string note = depositDialog.Note;
+
+                try
+                {
+                    if (_accountService.Deposit(_account.AccountId, amount, note))
+                    {
+                        MessageBox.Show("Deposit successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        DisplayAccountInfo();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid deposit amount.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error during deposit: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnDisplayTransactions_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var transactions = _accountService.GetAccountTransactions(_account.AccountId);
+                var transactionForm = new TransactionsForm(transactions);
+                transactionForm.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error retrieving transactions: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Cau 2/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace Cau_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblAccountId = new System.Windows.Forms.Label();
+            this.txtAccountId = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+
+            // lblAccountId
+            this.lblAccountId.AutoSize = true;
+            this.lblAccountId.Location = new System.Drawing.Point(12, 15);
+            this.lblAccountId.Name = "lblAccountId";
+            this.lblAccountId.Size = new System.Drawing.Size(61, 13);
+            this.lblAccountId.TabIndex = 0;
+            this.lblAccountId.Text = "Account ID:";
+
+            // txtAccountId
+            this.txtAccountId.Location = new System.Drawing.Point(80, 12);
+            this.txtAccountId.Name = "txtAccountId";
+            this.txtAccountId.Size = new System.Drawing.Size(192, 20);
+            this.txtAccountId.TabIndex = 1;
+
+            // lblPassword
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(12, 41);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(56, 13);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Password:";
+
+            // txtPassword
+            this.txtPassword.Location = new System.Drawing.Point(80, 38);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(192, 20);
+            this.txtPassword.TabIndex = 3;
+
+            // btnLogin
+            this.btnLogin.Location = new System.Drawing.Point(197, 64);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+
+            // LoginForm
+            this.AcceptButton = this.btnLogin;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 101);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtAccountId);
+            this.Controls.Add(this.lblAccountId);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bank System - Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private System.Windows.Forms.Label lblAccountId;
+        private System.Windows.Forms.TextBox txtAccountId;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
